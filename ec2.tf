fdb989154b5f9c4fc16f3887a0ef19bd04677e3c
StuAus <austinsallee@gmail.com>
2022-02-01 22:31:23 -0500

    updated AMI

--- ec2.tf
+++ ec2.tf
@@ -4,6 +4,6 @@ provider "aws"{
     secret_key = "MIbgx5NCbeeqMTPJebbtf/k6PIBhLzgqLjaAaf+N"
 }
 resource "aws_instance" "test-server"{
-    ami = "ami-0b6705f88b1f688c1"
+    ami = "ami-0a8b4cd432b1c3063"
     instance_type = "t2.micro"
 }

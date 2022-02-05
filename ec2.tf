ab6f4c796df99b227214a6a1521c4e20f222d1b4
StuAus <austinsallee@gmail.com>
2022-02-05 11:14:20 -0500

    Modified poc ami and sg for testing.

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "poc-instance" {
-  ami           = "ami-08895422b5f3aa64a"
+  ami           = "ami-0a8b4cd432b1c3063"
+  security_groups = [aws_security_group.asg_instance_sg.id]
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.dev-subnet-1.id
   tags = {

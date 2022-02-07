021edb80e36c38696fd587fffc37f12cd4e3a420
StuAus <austinsallee@gmail.com>
2022-02-06 23:10:22 -0500

    Formating changes.

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "poc-instance" {
-  ami           = "ami-0a8b4cd432b1c3063"
+  ami             = "ami-0a8b4cd432b1c3063"
   security_groups = [aws_security_group.asg_instance_sg.id]
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.dev-subnet-1.id
+  instance_type   = "t2.micro"
+  subnet_id       = aws_subnet.dev-subnet-1.id
   tags = {
     Name = "poc-instance"
   }

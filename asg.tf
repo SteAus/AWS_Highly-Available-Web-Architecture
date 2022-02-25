2b2326952b2e7c736e0fef261a1e7b5bf3c9b527
StuAus <austinsallee@gmail.com>
2022-02-25 01:54:20 -0500

    Set asg to use tomcat ami generated from packer.

--- asg.tf
+++ asg.tf
@@ -7,9 +7,17 @@ resource "aws_autoscaling_group" "dev-asg" {
     id = aws_launch_template.dev_launch_template.id
   }
 }
+data "aws_ami" "asg-instance-ami" {
+  most_recent = true
+  owners = ["self"]
+  filter {
+    name = "name"
+    values = ["tomcat-server"]
+  }
+}
 resource "aws_launch_template" "dev_launch_template" {
   name_prefix            = "dev-launch-template"
-  image_id               = "ami-00255b41d2c6e881b"
+  image_id               = data.aws_ami.asg-instance-ami.image_id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.asg_instance_sg.id]
 }

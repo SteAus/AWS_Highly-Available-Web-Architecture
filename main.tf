3a1a447c588ba40989649cc91c31f9760ace0220
StuAus <austinsallee@gmail.com>
2022-02-26 02:08:57 -0500

    Added vars to be passed from cmd line.

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+variable "access_key" {
+  type = string
+  default = ""
+}
+variable "secret_key" {
+  type = string
+  default = ""
+}
 provider "aws" {
   region     = "us-east-1"
   access_key = var.key
